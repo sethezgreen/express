b0a43b9d75c6164d577bdfac5a2948deca2a2669
sethezgreen <sethezgreen@gmail.com>
2024-01-15 14:11:07 -0500

    added findOne and delete functionality

--- jokes-api/server/controllers/joke.controller.js
+++ jokes-api/server/controllers/joke.controller.js
@@ -12,11 +12,35 @@ module.exports = { // exports all functions
             })
     },
 
+    // Find One Joke
+    findOneJoke: (req, res) => {
+        Joke.findOne({_id: req.params.id})
+            .then((oneJoke) => {
+                res.status(200).json(oneJoke)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
     // Create Joke (Create)
     createJoke: (req, res) => {
         Joke.create(req.body)
             .then((newJoke) => {
                 res.status(201).json(newJoke)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    // Update Joke
+
+    // Delete Joke
+    deleteJoke: (req, res) => {
+        Joke.deleteOne({_id: req.params.id})
+            .then((result) => {
+                res.status(201).json(result)
             })
             .catch((err) => {
                 res.status(500).json(err)
--- jokes-api/server/routes/joke.routes.js
+++ jokes-api/server/routes/joke.routes.js
@@ -3,4 +3,6 @@ const JokeController = require('../controllers/joke.controller')
 module.exports = (app) => {
     app.get('/api/allJokes', JokeController.findAllJokes)
     app.post('/api/createJoke', JokeController.createJoke)
+    app.get('/api/findOneJoke/:id', JokeController.findOneJoke)
+    app.delete('/api/deleteJoke/:id', JokeController.deleteJoke)
 }
